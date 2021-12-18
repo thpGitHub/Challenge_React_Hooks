0ffa808c1a33cfa9ebbe48d21cb9d28acbed48df
thierry <thierrypoupon@hotmail.com>
2021-12-18 23:32:50 +0100

    testing fetch on unsplash and style of buttons

--- src/Pages/DivPage.js
+++ src/Pages/DivPage.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./DivPage.css";
 import DivComponent from "../Components/DivComponent";
 
 export default function DivPages() {
   const [divsOrders, setDivsOrders] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [photos, setPhotos] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://api.unsplash.com/search/photos?query=t&client_id=TI96M4j8W_9hTLmsq2t883On8hdZx6cHKAoA_eBIJrE&per_page=8"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('datas',data)
+        setPhotos(data)
+        console.log(photos);
+        // console.log(data.results[0].urls.thumb);
+      });
+  },[]);
 
   const handleChangeDivOrderLeft = (indexDiv) => {
     const divsOrders2 = [...divsOrders];
@@ -21,6 +35,7 @@ export default function DivPages() {
     divsOrders2[findIndexAfter] -= 1;
     divsOrders2[indexDiv] += 1;
     setDivsOrders(divsOrders2);
+    console.log('photos',photos)
   };
 
   return (
@@ -32,6 +47,7 @@ export default function DivPages() {
           key={index + 1}
           onChangeDivOrderLeft={handleChangeDivOrderLeft}
           onChangeDivOrderRight={handleChangeDivOrderRight}
+          photo={photos}
         >
           élément {index + 1}
         </DivComponent>
